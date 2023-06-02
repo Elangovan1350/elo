f624e629a5251b25db488ae9168e47e5b2bbdb98
John Doe <johndoe@example.com>
2023-06-02 19:15:15 +0530

    ghhhhh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import {ClimbingBoxLoader, SyncLoader}  from "react-spinners"
 
 function App() {
+
+  const[loading , setloading]= useState(true)
+
+  useEffect(()=>{
+       console.log("useEffect runing");
+       setTimeout(()=>{
+            setloading(false)
+       },3000)
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        loading?
+        (<div className='loader'>
+          <ClimbingBoxLoader color='red' size={30} speedMultiplier={2}/>
+          <SyncLoader color="#36d7b7" />
+
+        </div>)
+        :(<h1>home page</h1>)
+      }
+     
     </div>
   );
 }
